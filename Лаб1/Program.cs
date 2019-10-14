0cef0959f4a32f228b5c083253cf02b956430d93
Finqie <43812704+Finqie@users.noreply.github.com>
2019-10-14 18:20:54 +0300

    Add files via upload

---
+++ Лаб1/Program.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = -1;
+            Console.Title = "Коваль Иван РТ5-31Б";
+            var korni = new List<double>();
+            while (i == -1)
+            {
+                Console.ResetColor();
+                double a, b, c, d;
+                Console.WriteLine("Введите коэффициент A: ");
+                a = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine("Введите коэффициент B: ");
+                b = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine("Введите коэффициент C: ");
+                c = Convert.ToDouble(Console.ReadLine());
+                Console.ForegroundColor = ConsoleColor.Green;
+                if ((a == 0) && (c == 0) && (b == 0))
+                {
+                    Console.WriteLine("Корней бесконечно много");
+                    Console.ReadKey();
+                    return;
+                }
+                else
+                {
+                    if ((a == 0) && (c == 0))
+                    {
+                        Console.WriteLine("0");
+                        Console.ReadKey();
+                        return;
+                    }
+                    else
+                    {
+                        if ((c == 0) && (b == 0))
+                        {
+                            Console.WriteLine("0");
+                            Console.ReadKey();
+                            return;
+                        }
+                    }
+                }
+
+                if ((a == 0) && (b == 0))
+                { }
+                else
+                {
+                    if (a == 0)
+                    {
+                        if ((c / b) <= 0)
+                        {
+                            korni.Add((Math.Sqrt(-c / b)));
+                            korni.Add((-Math.Sqrt(-c / b)));
+                        }
+                    }
+                    else
+                    {
+                        if (b == 0)
+                        {
+                            if ((c / a) <= 0)
+                            {
+                                korni.Add(Math.Sqrt(Math.Sqrt(-c / a)));
+                                korni.Add(-Math.Sqrt(Math.Sqrt(-c / a)));
+                            }
+                        }
+                        else
+                        {
+                            if (c != 0)
+                            {
+                                d = b * b - 4 * a * c;
+                                if (d >= 0)
+                                {
+                                    double xx = ((-b + Math.Sqrt(d)) / (2 * a));
+                                    double yy = ((-b - Math.Sqrt(d)) / (2 * a));
+                                    if (xx >= 0)
+                                    {
+                                        korni.Add(Math.Sqrt((-b + Math.Sqrt(d)) / (2 *
+                                       a)));
+                                        korni.Add(-Math.Sqrt((-b + Math.Sqrt(d)) / (2 *
+                                       a)));
+                                    }
+                                    if (yy >= 0)
+                                    {
+                                        korni.Add(Math.Sqrt((-b - Math.Sqrt(d)) / (2 *
+                                       a)));
+                                        korni.Add(-Math.Sqrt((-b - Math.Sqrt(d)) / (2 *
+                                       a)));
+                                    }
+                                }
+                            }
+                            else
+                            {
+                                if ((b / a) <= 0)
+                                {
+                                    korni.Add((Math.Sqrt(-b / a)));
+                                    korni.Add((-Math.Sqrt(-b / a)));
+                                }
+                                korni.Add(0);
+
+                            }
+                        }
+                    }
+                }
+
+                foreach (int itt in korni)
+                    i++;
+                if (i == -1)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Корней нет! Введите коэф. повторно");
+                }
+            }
+            int k = 1;
+            i = -1;
+            int i1 = 0;
+            foreach (int itt in korni)
+                i++;
+            Console.ForegroundColor = ConsoleColor.Green;
+            foreach (double itt in korni)
+            {
+                k = 0;
+                for (i1 = 0; i1 < i; i1++)
+                {
+                    if (korni[i1] == korni[i]) k++;
+                }
+                if (k == 0)
+                    Console.WriteLine(itt);
+            }
+            Console.ReadKey();
+        }
+    }
+}
